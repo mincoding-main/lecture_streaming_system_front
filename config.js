ea288a8b4c717c2efdda7cc2852634963194d609
Obong-B <ooollollollo@gmail.com>
2023-08-28 20:26:18 +0900

    Update .gitignore to exclude .history/

--- config.js
+++
@@ -1,9 +0,0 @@
-const getApiFrontPoint = () => process.env.NODE_ENV === 'production' ? process.env.FRONTEND_ADDRESS : process.env.FRONTEND_ADDRESS_DEV;
-const getApiBackPoint = () => process.env.NODE_ENV === 'production' ? process.env.BACKEND_ADDRESS : process.env.BACKEND_ADDRESS_DEV;
-
-const config = {
-    apiFrontpoint: getApiFrontPoint(),
-    apiBackPoint: getApiBackPoint(),
-};
-
-export default config;
