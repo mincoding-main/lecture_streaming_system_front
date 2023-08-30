b19dbd93d2b4035f9eae83f9b0dc97a7949741b1
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-08-30 16:57:55 +0900

    user-list search

--- pages/admin/user-list.js
+++ pages/admin/user-list.js
@@ -9,6 +9,12 @@ import AdminSideNavBar from '@/components/admin-side-navbar';
 import Pagination from '@mui/material/Pagination';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+
 
 
 export default function UserList() {
@@ -16,6 +22,10 @@ export default function UserList() {
     const [users, setUsers] = useState([]);
     const [page, setPage] = useState(1);
     const itemsPerPage = 8;
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [searchCategory, setSearchCategory] = useState('email'); // 기본 카테고리: Email
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -34,6 +44,54 @@ export default function UserList() {
         setPage(newPage);
     };
 
+    const handleSearchChange = (event) => {
+        const keyword = event.target.value;
+        setSearchKeyword(keyword);
+    };
+
+    const handleSearchCategoryChange = (event) => {
+        const category = event.target.value;
+        setSearchCategory(category);
+    };
+
+    const handleSearch = () => {
+        // 검색 버튼을 클릭했을 때의 로직을 구현하세요.
+        // 필터링된 결과를 사용하거나, 추가적인 API 호출 등을 수행할 수 있습니다.
+
+        // 카테고리에 따라 검색 필드를 선택합니다.
+        let searchField = '';
+        if (searchCategory === 'email') {
+            searchField = 'email';
+        } else if (searchCategory === 'employeeId') {
+            searchField = 'employeeId';
+        } else if (searchCategory === 'isAdmin') {
+            searchField = 'isAdmin';
+        } else if (searchCategory === 'id') {
+            searchField = 'id';
+        } else if (searchCategory === 'lectureId') {
+            searchField = 'lectureId';
+        } else {
+            // 유효한 카테고리가 아닌 경우, 기본 검색 필드를 선택합니다.
+            searchField = 'email';
+        }
+
+        // 검색 키워드와 선택된 카테고리에 따라 필터링된 사용자들을 구합니다.
+        const filteredUsers = users.filter(user => {
+            if (searchField === 'isAdmin') {
+                return user[searchField] === (searchKeyword.toLowerCase() === '관리자');
+            } else if (searchField === 'id') {
+                return user[searchField] === parseInt(searchKeyword);
+            } else {
+                return user[searchField].includes(searchKeyword);
+            }
+        });
+
+        // 필터링된 사용자 목록을 설정합니다.
+        setFilteredUsers(filteredUsers);
+
+        console.log('Filtered Users:', filteredUsers);
+    };
+
     const startIdx = (page - 1) * itemsPerPage;
     const endIdx = startIdx + itemsPerPage;
     return (
@@ -44,7 +102,29 @@ export default function UserList() {
                     <AdminSideNavBar />
                 </div>
                 <div className={adminCommonStyle.mainContainer}>
-                    <div className={adminUserDetailStyle.userSearchContainer}>검색창</div>
+                    <div className={adminUserDetailStyle.userSearchContainer}>
+                        <Button variant="contained" onClick={handleSearch}>
+                            검색
+                        </Button>
+                        <TextField
+                            className={adminUserDetailStyle.userSearchInput}
+                            label="검색어를 입력하세요"
+                            variant="outlined"
+                            value={searchKeyword}
+                            onChange={handleSearchChange}
+                            InputProps={{ sx: { height: '2.3rem' } }}
+                        />
+                        <FormControl className={adminUserDetailStyle.searchCategory}>
+                            <InputLabel>검색 카테고리</InputLabel>
+                            <Select value={searchCategory} onChange={handleSearchCategoryChange} style={{ height: '2.3rem' }}>
+                                <MenuItem value="id">회원 ID</MenuItem>
+                                <MenuItem value="email">Email</MenuItem>
+                                <MenuItem value="employeeId">사번</MenuItem>
+                                <MenuItem value="isAdmin">유저 권한</MenuItem>
+                                <MenuItem value="lectureId">강의 종류</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
                     <div className={adminUserDetailStyle.userContainer}>
                         <div className={adminUserDetailStyle.userItem}>
                             <div className={adminUserDetailStyle.userInfo}>
@@ -58,24 +138,45 @@ export default function UserList() {
                             </div>
                             <div className={adminUserDetailStyle.userEditBtnTitle} >정보 조회 / 수정</div>
                         </div>
-                        {users.slice(startIdx, endIdx).map(user => (
-                            <div key={user.id} className={adminUserDetailStyle.userItem}>
-                                <div className={adminUserDetailStyle.userInfo}>
-                                    <Grid container spacing={2}>
-                                        <Grid item xs={1}>{user.id}</Grid>
-                                        <Grid item xs={3}>{user.email}</Grid>
-                                        <Grid item xs={2}>{user.employeeId}</Grid>
-                                        <Grid item xs={3}>{user.isAdmin ? '관리자' : '사용자'}</Grid>
-                                        <Grid item xs={3}>{user.lectureId.join(', ')}</Grid>
-                                    </Grid>
+                        {(searchKeyword !== '' || searchCategory !== 'email') ? (
+                            filteredUsers.slice(startIdx, endIdx).map(user => (
+                                <div key={user.id} className={adminUserDetailStyle.userItem}>
+                                    <div className={adminUserDetailStyle.userInfo}>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={1}>{user.id}</Grid>
+                                            <Grid item xs={3}>{user.email}</Grid>
+                                            <Grid item xs={2}>{user.employeeId}</Grid>
+                                            <Grid item xs={3}>{user.isAdmin ? '관리자' : '사용자'}</Grid>
+                                            <Grid item xs={3}>{user.lectureId.join(', ')}</Grid>
+                                        </Grid>
+                                    </div>
+                                    <div className={adminUserDetailStyle.userEditBtn}>
+                                        <Button variant="contained">
+                                            수정
+                                        </Button>
+                                    </div>
                                 </div>
-                                <div className={adminUserDetailStyle.userEditBtn}>
-                                    <Button variant="contained">
-                                        수정
-                                    </Button>
+                            ))
+                        ) : (
+                            users.slice(startIdx, endIdx).map(user => (
+                                <div key={user.id} className={adminUserDetailStyle.userItem}>
+                                    <div className={adminUserDetailStyle.userInfo}>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={1}>{user.id}</Grid>
+                                            <Grid item xs={3}>{user.email}</Grid>
+                                            <Grid item xs={2}>{user.employeeId}</Grid>
+                                            <Grid item xs={3}>{user.isAdmin ? '관리자' : '사용자'}</Grid>
+                                            <Grid item xs={3}>{user.lectureId.join(', ')}</Grid>
+                                        </Grid>
+                                    </div>
+                                    <div className={adminUserDetailStyle.userEditBtn}>
+                                        <Button variant="contained">
+                                            수정
+                                        </Button>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))
+                        )}
                     </div>
                     <div className={adminUserDetailStyle.paginationContainer}>
                         <Pagination
