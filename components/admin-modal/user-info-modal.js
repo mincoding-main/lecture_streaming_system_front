b45b8da2ca7d58002d69769c2f1f9fae5632da28
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-08-31 14:37:04 +0900

    user list editModal field add and api edit

--- components/admin-modal/user-info-modal.js
+++ components/admin-modal/user-info-modal.js
@@ -1,28 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 import axios from 'axios';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+
 
 export default function AdminUserInfoModal({ open, onClose, user, onUpdateUser, onUpdateFilteredUsers }) {
-    const [updatedUser, setUpdatedUser] = useState({ ...user });
+    const [updatedUser, setUpdatedUser] = useState({ ...user, password: '' });
     const [errorMessage, setErrorMessage] = useState('');
+    const [lectures, setLectures] = useState([]);
+    const [selectedLectures, setSelectedLectures] = useState(new Set());
+
 
     useEffect(() => {
-        setUpdatedUser({ ...user });
+        setUpdatedUser({ ...user, password: '' });
         setErrorMessage('');
     }, [user])
+
+    // 전체 강의 가져오기
+    useEffect(() => {
+        const fetchLectures = async () => {
+            const response = await axios.get('/api/lectures');
+            setLectures(response.data);
+            setSelectedLectures(new Set(user.lectureId));
+        };
+        fetchLectures();
+    }, [user]);
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         setUpdatedUser(prevUser => ({ ...prevUser, [name]: value }));
     };
 
+    // 유저권한 업데이트
+    const handleRadioChange = (event) => {
+        const { name, value } = event.target;
+        const boolValue = value === 'true'; // 문자열을 불리언으로 변환
+        setUpdatedUser(prevUser => ({ ...prevUser, [name]: boolValue }));
+    };
+
+    // 강의 종류 업데이트
+    const handleCheckboxChange = (e) => {
+        const lectureId = parseInt(e.target.name);
+        const newSelectedLectures = new Set(selectedLectures);
+        if (newSelectedLectures.has(lectureId)) {
+            newSelectedLectures.delete(lectureId);
+        } else {
+            newSelectedLectures.add(lectureId);
+        }
+        const sortedLectures = Array.from(newSelectedLectures).sort((a, b) => a - b);
+        setSelectedLectures(new Set(sortedLectures));
+        setUpdatedUser(prevUser => ({ ...prevUser, lectureId: sortedLectures }));
+    };
+
+    // 전체 데이터 업데이트
     const handleUpdateUser = async () => {
         try {
-            const response = await axios.put(`/api/admin/users/${updatedUser.id}`, updatedUser);
+            const sortedLectures = Array.from(selectedLectures).sort((a, b) => a - b); // 정렬
+            let payload = { ...updatedUser, lectureId: sortedLectures };  // 정렬된 배열 사용
+
+
+            // 비밀번호가 빈 문자열인 경우 해당 필드를 제외합니다.
+            if (!updatedUser.password) {
+                const { password, ...rest } = updatedUser;
+                payload = { ...rest, lectureId: sortedLectures };  // 정렬된 배열 사용
+            }
+
+            const response = await axios.put(`/api/admin/users/${updatedUser.id}`, payload);
             console.log('User updated:', response.data);
             onUpdateUser(updatedUser); // 수정된 사용자 정보 전달
             onUpdateFilteredUsers(updatedUser); // 수정된 사용자 정보 업데이트
@@ -33,13 +85,14 @@ export default function AdminUserInfoModal({ open, onClose, user, onUpdateUser, 
         }
     };
 
+
     return (
         <Modal open={open} onClose={onClose}>
             <div style={{ position: 'absolute', top: '50%', left: '50%', width: '600px', height: 'auto', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px' }}>
                 <TextField
                     label="이메일"
                     name="email"
-                    value={updatedUser.email}
+                    value={updatedUser.email || ''}
                     onChange={handleInputChange}
                     variant="outlined"
                     fullWidth
@@ -48,13 +101,51 @@ export default function AdminUserInfoModal({ open, onClose, user, onUpdateUser, 
                 <TextField
                     label="사번"
                     name="employeeId"
-                    value={updatedUser.employeeId}
+                    value={updatedUser.employeeId || ''}
                     onChange={handleInputChange}
                     variant="outlined"
                     fullWidth
                     margin="normal"
                 />
-                {/* ... 나머지 사용자 정보 입력 필드 ... */}
+                <TextField
+                    label="새로운 비밀번호"
+                    name="password"
+                    value={updatedUser.password || ''}
+                    onChange={handleInputChange}
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                />
+                <FormControl component="fieldset" margin="normal" fullWidth>
+                    <FormLabel component="legend">유저 권한</FormLabel>
+                    <RadioGroup
+                        name="isAdmin"
+                        value={String(updatedUser.isAdmin)} // 불리언을 문자열로 변환
+                        onChange={handleRadioChange}
+                        row
+                    >
+                        <FormControlLabel value="true" control={<Radio />} label="관리자" />
+                        <FormControlLabel value="false" control={<Radio />} label="사용자" />
+                    </RadioGroup>
+                </FormControl>
+                <FormControl component="fieldset" margin="normal" fullWidth>
+                    <FormLabel component="legend">강의 종류</FormLabel>
+                    {lectures.map((lecture) => (
+                        <FormControlLabel
+                            key={lecture.id}
+                            control={
+                                <Checkbox
+                                    checked={selectedLectures.has(lecture.id)}
+                                    onChange={handleCheckboxChange}
+                                    name={String(lecture.id)}
+                                />
+                            }
+                            label={lecture.title}
+                        />
+                    ))}
+                </FormControl>
+
+
                 {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
                 <div style={{ display: 'flex', justifyContent: 'space-around' }}>
                     <Button variant="outlined" color='error' onClick={onClose} sx={{ width: '30%', marginTop: '1rem' }}>
--- pages/api/admin/users/[id].js
+++ pages/api/admin/users/[id].js
@@ -26,7 +26,7 @@ export default function handler(req, res) {
         const { id } = req.query;
 
         // 요청 바디에서 업데이트할 데이터 추출
-        const { employeeId, password } = req.body;
+        const { employeeId, password, isAdmin, lectureId } = req.body;  // lectureId 추가
 
         // JSON 파일을 불러와서 파싱
         const userData = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
@@ -37,7 +37,17 @@ export default function handler(req, res) {
         if (userIndex !== -1) {
             // 사용자 정보 업데이트
             userData[userIndex].employeeId = employeeId;
-            userData[userIndex].password = password;
+            userData[userIndex].isAdmin = isAdmin;
+
+            // 강의 ID 업데이트 (배열 형태라고 가정)
+            if (Array.isArray(lectureId)) {
+                userData[userIndex].lectureId = lectureId;
+            }
+
+            // 비밀번호가 빈 값이 아니면 업데이트
+            if (password && password.trim() !== '') {
+                userData[userIndex].password = password; // 새로운 비밀번호 업데이트
+            }
 
             // 업데이트된 사용자 정보를 파일에 저장
             fs.writeFileSync(usersFilePath, JSON.stringify(userData, null, 2), 'utf8');
@@ -47,8 +57,5 @@ export default function handler(req, res) {
             // 사용자 정보가 없으면 에러 메시지를 클라이언트로 전송
             res.status(404).json({ message: 'User not found' });
         }
-    } else {
-        // 지원하지 않는 메서드일 경우 에러 메시지를 클라이언트로 전송
-        res.status(405).json({ message: 'Method not allowed' });
     }
 }
