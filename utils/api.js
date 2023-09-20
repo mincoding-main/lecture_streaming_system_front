0875f6ea94d2d59455c2301b076bb1c2408edae6
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-09-20 14:03:16 +0900

    api annotation add

--- utils/api.js
+++ utils/api.js
@@ -4,9 +4,17 @@ import config from '@/config';
 const api = axios.create({
     baseURL: config.apiBackPoint
 });
+
+
 // Members
 
-// Create
+/**
+ * 새로운 멤버를 생성합니다.
+ *
+ * @param {Object} data - 새 멤버의 데이터.
+ * @returns {Promise<Object>} - 생성된 멤버의 데이터.
+ * @throws {Error} 생성 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const createMember = async (data) => {
     try {
         const response = await api.post(`/api/members`, data);
@@ -17,7 +25,13 @@ export const createMember = async (data) => {
     }
 };
 
-// Read (single)
+/**
+ * ID로 단일 멤버를 조회합니다.
+ *
+ * @param {string} id - 조회할 멤버의 ID.
+ * @returns {Promise<Object>} - 멤버의 데이터.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchMember = async (id) => {
     try {
         const response = await api.get(`/api/members/${id}`);
@@ -28,7 +42,12 @@ export const fetchMember = async (id) => {
     }
 };
 
-// Read (all)
+/**
+ * 모든 멤버를 조회합니다.
+ *
+ * @returns {Promise<Array<Object>>} - 모든 멤버의 배열.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchAllMembers = async () => {
     try {
         const response = await api.get(`/api/members`);
@@ -39,7 +58,14 @@ export const fetchAllMembers = async () => {
     }
 };
 
-// Update
+/**
+ * 멤버 정보를 업데이트합니다.
+ *
+ * @param {string} id - 업데이트할 멤버의 ID.
+ * @param {Object} data - 업데이트할 데이터.
+ * @returns {Object} 업데이트 성공 여부와 메시지를 담은 객체.
+ * @throws {Error} 업데이트 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const updateMember = async (id, data) => {
     try {
         const response = await api.put(`/api/members/${id}`, data);
@@ -50,7 +76,13 @@ export const updateMember = async (id, data) => {
     }
 };
 
-// Delete
+/**
+ * 멤버를 삭제합니다.
+ *
+ * @param {string} id - 삭제할 멤버의 ID.
+ * @returns {Promise<Object>} - 삭제된 멤버의 데이터.
+ * @throws {Error} 삭제 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const deleteMember = async (id) => {
     try {
         const response = await api.delete(`/api/members/${id}`);
@@ -63,7 +95,13 @@ export const deleteMember = async (id) => {
 
 // Lectures
 
-// Create
+/**
+ * 새로운 강의를 생성합니다.
+ *
+ * @param {Object} data - 새 강의의 데이터.
+ * @returns {Promise<Object>} - 생성된 강의의 데이터.
+ * @throws {Error} 생성 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const createLecture = async (data) => {
     try {
         const response = await api.post(`/api/lectures`, data);
@@ -74,7 +112,13 @@ export const createLecture = async (data) => {
     }
 };
 
-// Read (single)
+/**
+ * ID로 단일 강의를 조회합니다.
+ *
+ * @param {string} id - 조회할 강의의 ID.
+ * @returns {Promise<Object>} - 강의의 데이터.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchLecture = async (id) => {
     try {
         const response = await api.get(`/api/lectures/${id}`);
@@ -85,7 +129,12 @@ export const fetchLecture = async (id) => {
     }
 };
 
-// Read (all)
+/**
+ * 모든 강의를 조회합니다.
+ *
+ * @returns {Promise<Array<Object>>} - 모든 강의의 배열.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchAllLectures = async () => {
     try {
         const response = await api.get(`/api/lectures`);
@@ -96,7 +145,14 @@ export const fetchAllLectures = async () => {
     }
 };
 
-// Update
+/**
+ * 강의 정보를 업데이트합니다.
+ *
+ * @param {string} id - 업데이트할 강의의 ID.
+ * @param {Object} data - 업데이트할 데이터.
+ * @returns {Promise<Object>} - 업데이트된 강의의 데이터.
+ * @throws {Error} 업데이트 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const updateLecture = async (id, data) => {
     try {
         const response = await api.put(`/api/lectures/${id}`, data);
@@ -107,7 +163,13 @@ export const updateLecture = async (id, data) => {
     }
 };
 
-// Delete
+/**
+ * 강의를 삭제합니다.
+ *
+ * @param {string} id - 삭제할 강의의 ID.
+ * @returns {Promise<Object>} - 삭제된 강의의 데이터.
+ * @throws {Error} 삭제 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const deleteLecture = async (id) => {
     try {
         const response = await api.delete(`/api/lectures/${id}`);
@@ -121,7 +183,14 @@ export const deleteLecture = async (id) => {
 
 // Lecture Videos
 
-// Create
+/**
+ * 새로운 강의 비디오를 생성합니다.
+ *
+ * @param {string} lectureId - 강의 ID.
+ * @param {Object} data - 새 비디오의 데이터.
+ * @returns {Promise<Object>} 생성된 비디오의 데이터.
+ * @throws {Error} 생성 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const createLectureVideo = async (lectureId, data) => {
     try {
         const response = await api.post(`/api/lectures/${lectureId}/videos`, data);
@@ -132,7 +201,14 @@ export const createLectureVideo = async (lectureId, data) => {
     }
 };
 
-// Read (single)
+/**
+ * ID로 단일 강의 비디오를 조회합니다.
+ *
+ * @param {string} lectureId - 강의 ID.
+ * @param {string} videoId - 비디오 ID.
+ * @returns {Promise<Object>} 조회된 비디오의 데이터.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchLectureVideo = async (lectureId, videoId) => {
     try {
         const response = await api.get(`/api/lectures/${lectureId}/videos/${videoId}`);
@@ -143,7 +219,13 @@ export const fetchLectureVideo = async (lectureId, videoId) => {
     }
 };
 
-// Read (all)
+/**
+ * 하나의 강의에 포함된 모든 비디오를 조회합니다.
+ *
+ * @param {string} lectureId - 강의 ID.
+ * @returns {Promise<Array<Object>>} 모든 비디오의 배열.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchAllLectureVideos = async (lectureId) => {
     try {
         const response = await api.get(`/api/lectures/${lectureId}/videos`);
@@ -154,7 +236,15 @@ export const fetchAllLectureVideos = async (lectureId) => {
     }
 };
 
-// Update
+/**
+ * 강의 비디오 정보를 업데이트합니다.
+ *
+ * @param {string} lectureId - 강의 ID.
+ * @param {string} videoId - 비디오 ID.
+ * @param {Object} data - 업데이트할 데이터.
+ * @returns {Promise<Object>} 업데이트된 비디오의 데이터.
+ * @throws {Error} 업데이트 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const updateLectureVideo = async (lectureId, videoId, data) => {
     try {
         const response = await api.put(`/api/lectures/${lectureId}/videos/${videoId}`, data);
@@ -165,7 +255,14 @@ export const updateLectureVideo = async (lectureId, videoId, data) => {
     }
 };
 
-// Delete
+/**
+ * 강의 비디오를 삭제합니다.
+ *
+ * @param {string} lectureId - 강의 ID.
+ * @param {string} videoId - 비디오 ID.
+ * @returns {Promise<Object>} 삭제된 비디오의 데이터.
+ * @throws {Error} 삭제 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const deleteLectureVideo = async (lectureId, videoId) => {
     try {
         const response = await api.delete(`/api/lectures/${lectureId}/videos/${videoId}`);
@@ -180,7 +277,13 @@ export const deleteLectureVideo = async (lectureId, videoId) => {
 
 // Tags
 
-// Create
+/**
+ * 새로운 태그를 생성합니다.
+ *
+ * @param {Object} data - 새 태그의 데이터.
+ * @returns {Promise<Object>} 생성된 태그의 데이터.
+ * @throws {Error} 생성 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const createTag = async (data) => {
     try {
         const response = await api.post(`/api/tags`, data);
@@ -191,7 +294,13 @@ export const createTag = async (data) => {
     }
 };
 
-// Read (single)
+/**
+ * ID로 단일 태그를 조회합니다.
+ *
+ * @param {string} id - 태그 ID.
+ * @returns {Promise<Object>} 조회된 태그의 데이터.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchTag = async (id) => {
     try {
         const response = await api.get(`/api/tags/${id}`);
@@ -202,7 +311,12 @@ export const fetchTag = async (id) => {
     }
 };
 
-// Read (all)
+/**
+ * 모든 태그를 조회합니다.
+ *
+ * @returns {Promise<Array<Object>>} 모든 태그의 배열.
+ * @throws {Error} 조회 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const fetchAllTags = async () => {
     try {
         const response = await api.get(`/api/tags`);
@@ -213,7 +327,14 @@ export const fetchAllTags = async () => {
     }
 };
 
-// Update
+/**
+ * 태그 정보를 업데이트합니다.
+ *
+ * @param {string} id - 태그 ID.
+ * @param {Object} data - 업데이트할 데이터.
+ * @returns {Promise<Object>} 업데이트된 태그의 데이터.
+ * @throws {Error} 업데이트 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const updateTag = async (id, data) => {
     try {
         const response = await api.put(`/api/tags/${id}`, data);
@@ -224,7 +345,13 @@ export const updateTag = async (id, data) => {
     }
 };
 
-// Delete
+/**
+ * 태그를 삭제합니다.
+ *
+ * @param {string} id - 태그 ID.
+ * @returns {Promise<Object>} 삭제된 태그의 데이터.
+ * @throws {Error} 삭제 중에 에러가 발생하면 예외를 던집니다.
+ */
 export const deleteTag = async (id) => {
     try {
         const response = await api.delete(`/api/tags/${id}`);
