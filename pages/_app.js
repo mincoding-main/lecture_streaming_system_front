e7a553317523505d815ab59eef666aeada6f7183
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-09-22 09:44:22 +0900

    matarial ul font setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,21 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/utils/theme';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="pageContainer">
-      <Header />
-      <main className="mainContent">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="pageContainer">
+        <Header />
+        <main className="mainContent">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
-
