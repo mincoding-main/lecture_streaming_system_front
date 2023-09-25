8fc131b85a38a2210f245bdb8f352f13cac75762
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-09-25 16:44:39 +0900

    lecture list item 0 item and css edit

--- components/lecture-list-item.js
+++ components/lecture-list-item.js
@@ -4,6 +4,7 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import { useRouter } from 'next/router';
 import lectureListDetailStyle from '@/styles/main/lecture-list-detail.module.css'
@@ -14,27 +15,34 @@ export default function LectureListCard({ lectureData }) {
     console.log(lectureData)
     return (
         <List className={lectureListDetailStyle.listContainer}>
-            {lectureData.map((lecture) => (
-                <ListItem
-                    key={lecture.lecture.id}
-                    component="div"
-                    disablePadding
-                    className={lectureListDetailStyle.listItem}
-                >
-                    <ListItemButton
-                        onClick={() => router.push(`/lecture/lecture-detail/${lecture.lecture.id}`)}
-                        className={lectureListDetailStyle.listItemBtn}
+            {lectureData && lectureData.length > 0 ? (
+                lectureData.map((lecture) => (
+                    <ListItem
+                        key={lecture.lecture.id}
+                        component="div"
+                        disablePadding
+                        className={lectureListDetailStyle.listItem}
                     >
-                        <CardMedia
-                            component="img"
-                            image={`${baseURL}/${lecture.lecture.img.image_name}`}
-                            alt={lecture.lecture.title}
-                            className={lectureListDetailStyle.cardMedia}
-                        />
-                        <ListItemText primary={lecture.lecture.title} secondary={lecture.lecture.content} />
-                    </ListItemButton>
-                </ListItem>
-            ))}
+                        <ListItemButton
+                            onClick={() => router.push(`/lecture/lecture-detail/${lecture.lecture.id}`)}
+                            className={lectureListDetailStyle.listItemBtn}
+                        >
+                            <CardMedia
+                                component="img"
+                                image={`${baseURL}/${lecture.lecture.img.image_name}`}
+                                alt={lecture.lecture.title}
+                                className={lectureListDetailStyle.cardMedia}
+                            />
+                            <ListItemText primary={lecture.lecture.title} secondary={lecture.lecture.content} />
+                        </ListItemButton>
+                    </ListItem>
+                ))
+            ) : (
+                <div className={lectureListDetailStyle.listItem}>
+                    <Typography>수강 신청 된 강의가 존재하지 않습니다.</Typography>
+                </div>
+            )}
+
         </List>
     );
 }
