e7a42f5863a36fd53845fe2d51eea5311c82b1ef
Seongeun Byun <seongeun.byun@mincoding.co.kr>
2023-09-26 11:07:20 +0900

    ckeditor comment delete

--- pages/lecture/lecture-streaming/[lectureId]/[videoId].js
+++ pages/lecture/lecture-streaming/[lectureId]/[videoId].js
@@ -4,10 +4,6 @@ import { useRouter } from 'next/router';
 import { useAuthCheck } from '@/utils/auth-check'
 import { Button, List, ListItem, ListItemText, Paper } from '@mui/material';
 import lectureStreamingStyle from '@/styles/main/lecture-streaming.module.css';
-// ckEditor 부분
-// import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import ClassicEditorBuild from '@ckeditor/ckeditor5-build-classic';
-
 
 export default function LectureStreaming() {
     const router = useRouter();
@@ -15,10 +11,6 @@ export default function LectureStreaming() {
     const { videoId } = router.query;
     const [tagetVideoItem, setTagetVideoItem] = useState(null);
     const [tagetVideoAllItems, setTagetVideoAllItems] = useState([]);
-
-    // ckEditor 부분
-    // const [editMode, setEditMode] = useState(false);
-    // const [isAdmin, setIsAdmin] = useState(false); // 사용자의 권한 여부를 나타내는 상태 변수
 
     useAuthCheck(true, true, true);
 
@@ -53,15 +45,6 @@ export default function LectureStreaming() {
         return <div>Loading...</div>;
     }
 
-    // ckEditor 부분
-    // const toggleEditMode = () => {
-    //     setEditMode(!editMode);
-    // };
-
-    // const handleSave = () => {
-    //     setEditMode(false);
-    // };
-
     return (
         <>
             <section className={lectureStreamingStyle.streamingIntroSection}>
@@ -80,44 +63,6 @@ export default function LectureStreaming() {
                                 allow="autoplay"
                             />
                         </div>
-                        {/*ckEditor 부분 나중에 필요하면 추가*/}
-                        {/* {editMode ? (
-                            <div className={lectureStreamingStyle.streamingVideo}>
-                                <CKEditor
-                                    style={{ height: '300px' }}
-                                    editor={ClassicEditorBuild}
-                                    data={editedContent + "&autoplay=1"}
-                                    onChange={(event, editor) => {
-                                        const data = editor.getData();
-                                        setEditedContent(data);
-                                    }}
-                                />
-                            </div>
-                        ) : (
-                            <div className={lectureStreamingStyle.streamingVideo}>
-                                <iframe
-                                    src={editedContent + "&autoplay=1"}
-                                    width="100%"
-                                    height="100%"
-                                    frameBorder="0"
-                                    allowFullScreen
-                                    allow="autoplay"
-                                />
-                            </div>
-                        )}
-                        {isAdmin && (
-                            <div>
-                                {editMode ? (
-                                    <Button variant="contained" onClick={handleSave} className={lectureStreamingStyle.editModeBtn}>
-                                        저장
-                                    </Button>
-                                ) : (
-                                    <Button variant="contained" onClick={toggleEditMode} className={lectureStreamingStyle.editModeBtn}>
-                                        수정
-                                    </Button>
-                                )}
-                            </div>
-                        )} */}
                     </div>
                     <div className={lectureStreamingStyle.listContainer}>
                         <Paper elevation={3} className={lectureStreamingStyle.listPaper}>
